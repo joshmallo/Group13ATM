1f21a0b0ec7541963ec7e1c2d2ffbb14c4b2670e
dylanwares <c1012044@my.shu.ac.uk>
2022-11-10 15:48:13 +0000

    test

--- Group13ATM/Program.cs
+++ Group13ATM/Program.cs
@@ -17,6 +17,7 @@ namespace Group13ATM
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            Console.WriteLine("hello");
         }
     }
 }
