c55ddf3a0d780bad120a39c08392dec824ba379e
Josh264 <c0014437@my.shu.ac.uk>
2022-12-01 16:47:45 +0000

    When you want to change db file location, change in functions.cs for both

---
+++ Group13ATM/Functions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Group13ATM
+{
+    class Functions
+    {
+        public static string pathToDB()
+        {
+            return @"data source=C:\Users\USER\Desktop\Year2SHU\IntroToSoft\Project\Group13ATM\Databases\Employee.db";
+        }
+    }
+}
